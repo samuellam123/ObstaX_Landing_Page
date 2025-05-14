eabdc22ae4da107b5b4631662a3611eafa974e42
Samuel Lam <samuellam123456@hotmail.com>
2025-05-14 19:34:03 +0800

    reinitialise repo

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,21 +1,31 @@
 import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// SEO Metadata
 export const metadata: Metadata = {
   title: "ObstaX",
-  description: "Landing Page",
+  description: "Assistive Navigation Device for the Blind and Visually Impaired in Singapore",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>
-        <h1>HI</h1>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
