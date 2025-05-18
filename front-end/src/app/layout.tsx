99e40c135151e177465c379000105144c31386cb
Samuel Lam <samuellam123456@hotmail.com>
2025-05-18 22:46:30 +0800

    [Feature] Basic Layout #1: Header

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-w-[375px]">
         <Header />
         {children}
       </body>
--- front-end/src/components/layout/Header.tsx
+++ front-end/src/components/layout/Header.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { Menu } from 'lucide-react';
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-50 bg-slate-300 w-full px-4 py-4">
+        <header className="sticky top-0 z-50 bg-slate-300 w-full px-8 py-4">
             <div className="flex justify-between items-center max-w-2xl mx-auto w-full">
 
                 <Link href="/" className="text-xl font-bold">
@@ -11,11 +27,11 @@ export default function Header() {
                 </Link>
 
                 <div className="flex items-center space-x-6">
-                    <Link href="/product" className="text-sm font-medium">
+                    <Link href="/product" className="text-sm font-medium hidden sm:flex">
                         How it works
                     </Link>
 
-                    <Link href="/about" className="text-sm font-medium">
+                    <Link href="/about" className="text-sm font-medium hidden sm:flex">
                         Who are we
                     </Link>
 
@@ -24,8 +40,25 @@ export default function Header() {
                             Pre-Order
                         </Link>
                     </Button>
-                </div>
 
+                    <div className="sm:hidden">
+                        <DropdownMenu modal={false}>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="outline" size="icon">
+                                    <Menu />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-40 justify-end" align="end" >
+                                <DropdownMenuItem asChild className="cursor-pointer">
+                                    <Link href="/product">How it works</Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem asChild className="cursor-pointer">
+                                    <Link href="/about">Who are we</Link>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                </div>
             </div>
         </header>
     )
